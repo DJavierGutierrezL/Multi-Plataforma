0d0b19f595829155ea744aa9b886f3794e4c4b8c
DarwinGuti85 <tu_correo_asociado_a_github@ejemplo.com>
2025-09-10 11:31:49 -0500

    haciendo arreglos1.1

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const prisma = require('../prismaClient');
 const { sign } = require('../utils/jwt');
 
